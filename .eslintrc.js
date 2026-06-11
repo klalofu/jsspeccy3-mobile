97bd607bebe319e4a2a2ae81ca92acc13617d8e8
Klalofudo Klalofu <klalofudo@gmail.com>
2026-06-11 13:41:39 +0300

    linter added

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["html"],
+  rules: {
+    "no-console": "warn",
+    "no-alert": "warn",
+  },
+};
